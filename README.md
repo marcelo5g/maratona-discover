15ae383859476e60c6fb5b2c7c05db748ad64200
Marcelo Martins <m.martinsx@gmail.com>
2021-02-21 15:52:36 -0300

    :tada: First Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+<h1 align="center">
+  <img alt="dev.finances" title="dev.finances" src="./assets/logo.svg" width="220px" />
+</h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-Milha Extra">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="dev.finances" src="./images/screenshot.png" width="50%">
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+
+<br>
+
+
+## 💻 Projeto
+
+O dev.finances é uma ideia de aplicação para controle de finanças pessoais. Nele é possível incluir e excluir transações, informando a descrição do pagamento ou despesa, seu valor e a data da operação. Por fim, é apresentado o balanço geral e o total de dinheiro movimentado de entradas e saídas.
+
+
+<br>
+
+## Milha Extra
+
+Foi adicionada uma pequena animação de entrada aos cards com os valores de Entrada, Saída e Total.
+
+
+<br>
+
+
+---
+
+Feito com ♥ by Rocketseat :wave: [Participe da nossa comunidade!](https://discordapp.com/invite/gCRAFhc)
---
+++ scripts.js
@@ -0,0 +1,296 @@
+const Modal = {
+    open(){
+        // Abrir o Modal
+        // Adicionar a class active ao modal
+        document
+            .querySelector(".modal-overlay")
+            .classList
+            .add('active')
+    },
+    close(){
+        // Fechar o Modal
+        // Remover a class active do modal
+        document
+            .querySelector(".modal-overlay")
+            .classList
+            .remove('active')
+    }
+}
+
+const Storage = {
+    get() {
+        // JSON.parse transforma a string de volta para array (se não encontrar o localStorage, devolve um array vazio)
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+    },
+
+    set(transactions) {
+        //Storage armazena strings, portanto preciso converter o meu array 'transactions' numa string através da funcionalidade do objeto JSON.stringify()
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+
+    }
+}
+
+
+const Transaction = {
+    all: Storage.get(),
+    
+    // [
+    //     {
+    //         description: 'Luz',
+    //         amount: -50001,
+    //         date: '23/01/2021',
+    //     },
+    //     {
+    //         description: 'Website',
+    //         amount: 500000,
+    //         date: '23/01/2021',
+    //     },
+    //     {
+    //         description: 'Internet',
+    //         amount: -20012,
+    //         date: '23/01/2021',
+    //     },
+    //     {
+    //         description: 'App',
+    //         amount: 200000,
+    //         date: '23/01/2021',
+    //     },
+    // ],
+
+    add(transaction){
+        Transaction.all.push(transaction)
+
+        App.reload()        
+    },
+
+    remove(index) {
+        // splice = deleta do array o item indicado no index
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+
+    },
+
+
+    incomes() {
+        let income = 0;
+        // Selecionar todas as transacoes
+        // para cada transacao,
+        Transaction.all.forEach((transaction) => {
+            // se for maior que zero
+            if (transaction.amount > 0) {
+                // somar a uma variavel
+                income += transaction.amount;
+            }
+        })
+        // retornar a variavel
+        return income; 
+    },
+
+    expenses() {
+        let expense = 0;
+        // Selecionar todas as transacoes
+        // para cada transacao,
+        Transaction.all.forEach((transaction) => {
+            // se for menor que zero
+            if (transaction.amount < 0) {
+                // somar a uma variavel
+                expense += transaction.amount;
+            }
+        })
+        // retornar a variavel
+        return expense;
+    },
+
+    total() {
+        return Transaction.incomes() + Transaction.expenses()
+    }
+}
+
+
+// Substituir os dados do HTML com os dados do JS
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
+
+        DOM.transactionsContainer.appendChild(tr)
+
+    },
+
+    innerHTMLTransaction(transaction, index) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = ` 
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}"> ${amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td >
+            <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
+        </td>
+        `
+
+        return html
+    },
+
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.total())
+
+    },
+
+    clearTransactions(){
+        DOM.transactionsContainer.innerHTML = ""
+
+    }
+}
+
+const Utils = {
+    formatAmount(value){
+        value = Number(value) * 100
+
+        // value = Number(value.replace(/\,\./g, "")) * 100      
+
+        return value
+    },
+
+    formatDate(date){
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+        // "/\D/g" É uma expressão regular (REgex) que irá procurar globalmente tudo que não for um número e trocar por nada
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+    }
+}
+
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+    
+
+    validateFields(){        
+        const { description, amount, date } = Form.getValues()
+
+        // .trim = Retira teclas de Espaço
+        if(description.trim() === "" || amount.trim() === "" || date.trim() === "") {
+            throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+
+    formatValues(){
+        let { description, amount, date } = Form.getValues()
+
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    saveTransaction(transaction){
+        Transaction.add(transaction)
+    },
+
+    clearFields(){
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
+
+    
+    submit(event) {
+        event.preventDefault()
+
+        try {
+            // verificar se todas as informações foram preenchidas
+            Form.validateFields()
+
+            // formatar os dados para salvar
+            const transaction = Form.formatValues()
+
+            // salvar (No Transaction.add já tem um Reload())
+            Form.saveTransaction(transaction)
+
+            // apagar os dados do formulário
+            Form.clearFields()
+
+            // fechar o modal
+            Modal.close()
+
+        } catch (error) {
+            alert(error.message)
+            
+        }   
+
+        
+
+    }
+}
+
+
+
+
+const App = {
+    init(){
+
+        Transaction.all.forEach(function(transaction, index){
+            DOM.addTransaction(transaction, index)
+        })
+        
+        DOM.updateBalance()  
+        
+        Storage.set(Transaction.all)
+        
+    },
+
+    reload(){
+        DOM.clearTransactions()
+        App.init()
+    },
+
+}
+
+App.init()
+
+
+
